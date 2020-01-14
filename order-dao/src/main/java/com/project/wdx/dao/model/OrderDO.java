b090e4e30e04fbb9a214ad70e799db06a4df07af
wb-wdx330446 <wb-wdx330446@antfin.com>
2020-01-14 18:35:25 +0800

    1

--- order-dao/src/main/java/com/project/wdx/dao/model/OrderDO.java
+++ order-dao/src/main/java/com/project/wdx/dao/model/OrderDO.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @Table(name = "order_tbl")
 public class OrderDO {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "user_id")
--- order-facade/src/main/java/com/project/wdx/model/Order.java
+++ order-facade/src/main/java/com/project/wdx/model/Order.java
@@ -3,10 +3,11 @@ package com.project.wdx.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
-public class Order {
+public class Order implements Serializable{
 
     private Long id;
 
